4cea6f30b97c3da0f9c55fa9b304da0a715655fb
qianshuwanhua <qianshuwanhua@qq.com>
2022-07-18 22:12:29 +0800

    master01

---
+++ chapter01/src/main/java/com/huawei/Solution.java
@@ -0,0 +1,14 @@
+package com.huawei;
+
+/**
+ * @ClassName Solution
+ * @Author Huanglong
+ * @Date 2022/7/18 22:10
+ * @Description Solution
+ * @Version 1.0
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
