d6222ccca416cd49968a78dc6942f9b8413b90a3
qianshuwanhua <qianshuwanhua@qq.com>
2022-07-18 22:29:10 +0800

    master01

---
+++ helloworld/src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Solution
+ * @Author Huanglong
+ * @Date 2022/7/18 22:27
+ * @Description Solution
+ * @Version 1.0
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
